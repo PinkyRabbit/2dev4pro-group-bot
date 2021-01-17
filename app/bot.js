3155b1d4ade13534508831f2b4dd033f6d9a3557
Mikita Melnikau <usesa@yandex.com>
2021-01-17 12:08:04 +0300

    ping-pong for bot added

--- app/bot.js
+++ app/bot.js
@@ -4,6 +4,18 @@ const token = process.env.TELEGRAM_BOT_KEY;
 
 function initTelegramBot(callback) {
   const bot = new TelegramBot(token, {polling: true});
+
+  bot.on('message', (message) => {
+    const chatId = message.chat.id;
+
+    if (message.text === 'ping') {
+      return bot.sendMessage(chatId, 'Pong 🏓');
+    }
+
+    // send a message to the chat acknowledging receipt of their message
+    bot.sendMessage(chatId, 'Received your message');
+  });
+
   return callback(bot);
 }
 
