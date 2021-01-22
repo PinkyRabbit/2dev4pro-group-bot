07fdd848e4f5e1030e1d0c23fbe35afdf28b6b0a
Mikita Melnikau <usesa@yandex.com>
2021-01-22 17:59:01 +0300

    helmet import fix

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ const Koa = require('koa');
 const Router = require('@koa/router');
 const cors = require('@koa/cors');
 const bodyParser = require('koa-bodyparser');
+const helmet = require('koa-helmet');
 
 const { handlerFor } = require('./handlers');
 const { initErrorHandler } = require('./error-handler');
