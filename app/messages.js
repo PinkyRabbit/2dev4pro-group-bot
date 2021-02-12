a34acdd2b94522ab9a83fba3c9749300786f8ea5
Mikita Melnikau <usesa@yandex.com>
2021-02-12 17:09:39 +0300

    messages for users without username

--- app/messages.js
+++ app/messages.js
@@ -1,13 +1,20 @@
 const messageFor = {
-  reward: (username, title, description) => [
-    `💥 @${username}, поздравляем!`,
-    '---',
-    `Вы получили награду: *${title}*🎖`,
-    ' ',
-    `_${description}_`,
-  ].join('\n'),
+  reward: (username, title, description) => 
+    username
+    ? [
+      `💥 @${username}, поздравляем!`,
+      '---',
+      `Вы получили награду: *${title}*🎖`,
+      ' ',
+      `_${description}_`,
+    ].join('\n')
+    : [
+      `💥 Пользователь *со скрытым ником* получает награду: *${title}*🎖`,
+      ' ',
+      `_${description}_`,
+    ].join('\n'),
   topic: (username, topic) => [
-    `Пользователь @${username} создал новый раздел *${topic}*.`,
+    `Пользователь ${username ? `@${username}` : '(Имя скрыто)'} создал новый раздел *${topic}*.`,
     '🥶  🎃  🥶  🎃  🥶',
   ].join('\n'),
 };
